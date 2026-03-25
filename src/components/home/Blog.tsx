8afa9b8f61bfd05083806fae280e9522d081bd47
KazouDev <frenchkazou@gmail.com>
2026-03-25 13:46:21 +0100

    cache / no article

--- src/components/home/Blog.tsx
+++ src/components/home/Blog.tsx
@@ -4,15 +4,24 @@ import { CardArticle } from "../articles/CardArticle";
 
 const ARTICLE_LIMIT = 12;
 
+export const revalidate = 0;
+
+
 export default async function Blog() {
     const { articles } = await getArticles(ARTICLE_LIMIT);
     return (
         <section id="blog">
             <h2 className="section" style={{ paddingBottom: '5vh' }}>BLOG</h2>
             <ul className="blog-grid">
-                {articles.map((article) => (
-                    <CardArticle key={article.id} article={article} />
-                ))}
+                {
+                    articles.length === 0 ? (
+                        <p>Aucun article disponible.</p>
+                    ) : (
+                        articles.map((article) => (
+                            <CardArticle key={article.id} article={article} />
+                        ))
+                    )
+                }
             </ul>
 
             <Link href="/blog" className="ListeBlog">
